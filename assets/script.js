3cbb801da22415a4fa541378958c5a3160fbefde
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2023-05-09 20:33:18 +0200

    Adding dots from js

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,27 @@
 const slides = [
 	{
 		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>",
+		"dot": true,
+		"dot_selected" : true
 	},
 	{
 		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+		"dot": true,
+		"dot_selected" : false
 	},
 	{
 		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>",
+		"dot": true,
+		"dot_selected" : false
 	},
 	{
 		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>",
+		"dot": true,
+		"dot_selected" : false
 	}
 ]
 
@@ -21,7 +29,6 @@ const arrowLeft 	= document.querySelector(".arrow_left");
 const arrowRight 	= document.querySelector(".arrow_right");
 
 // Permet d'acceder au tableau
-// const slideImage	= slides[0];
 
 // Permet de récuperer la classe 
 const bannerImage 			= document.querySelector('.banner-img');
@@ -30,6 +37,18 @@ bannerImage.src 			= "./assets/images/slideshow/" + slides[numberIndex].image;
 
 const bannerParagraph 		= document.querySelector('#banner p')
 bannerParagraph.innerHTML 	= slides[numberIndex].tagLine;
+	
+
+for (let i = 0; i < 4; i++) {
+	
+	const dotElement				= document.createElement("div");
+	dotElement.classList.add("dot");
+	
+	const dots						= document.querySelector(".dots");
+	dots.appendChild(dotElement)
+}
+
+
 
 
 function arrowleft() {
