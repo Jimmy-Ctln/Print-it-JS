caf50fd2b251e3016bb7710112bfbda7d8fdc5a2
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2023-05-09 12:30:27 +0200

    Step 2: Add event click arrows

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,16 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+
+arrowLeft.addEventListener('click', () => {
+
+	console.log('click arrow left');
+})
+
+arrowRight.addEventListener('click', () => {
+
+	console.log('click arrow right');
+})
